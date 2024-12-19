833524e11738f45499fc6d5aaf4839c66b35dd03
kishore <ramkishoreengineer@gmail.com>
2024-12-19 21:25:58 +0530

    added testng file

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,2 +1,10 @@
+import org.testng.annotations.Test;
+
 public class SampleTest {
+
+    @Test
+    public static void firstTestcase(){
+        System.out.println("running successfully");
+        System.out.println("configured successfully");
+    }
 }
