24982dbe354aff95858baebb9290f7c6ee358f4b
kishore <ramkishoreengineer@gmail.com>
2024-12-27 16:19:38 +0530

    added test case 1

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,11 +1,22 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class SampleTest {
 
     @Test
     public static void firstTestcase(){
-        System.out.println("running successfully");
-        System.out.println("configured successfully");
-         System.out.println("added in repo");
+       WebDriver driver = new ChromeDriver();
+       driver.get("https://demoqa.com/");
+       driver.manage().window().maximize();
+       driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
+       String title = driver.getTitle();
+      Assert.assertEquals(title,"DEMOQA");
+      driver.quit();
+
     }
 }
